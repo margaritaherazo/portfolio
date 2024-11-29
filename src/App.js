e17e8a97f4d5782e52b3866c1d9cea67ff5d3924
maggi <margarita.herazo.i@gmail.com>
2024-11-29 11:54:28 +0100

    addind example about

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react';
 import Hero from './components/Hero';
 import About from './components/About';
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+    return (
+        <section id="about" className="about bg-light py-5">
+            <div className="container">
+                <h2>About Me</h2>
+                <p>I am a passionate Front-End Developer with a knack for creating user-friendly and visually appealing websites.</p>
+            </div>
+        </section>
+    );
+}
+
+export default About;
