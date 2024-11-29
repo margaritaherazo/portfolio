a70c8f7434ff81df4bd64e7211a8580dfcc1ce2e
maggi <margarita.herazo.i@gmail.com>
2024-11-29 16:07:54 +0100

    adjusting navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,39 +1,46 @@
 import React, { useState } from 'react';
 import { Navbar, Nav, Button } from 'react-bootstrap';
-import { Link } from 'react-scroll'; 
-
+import { Link } from 'react-scroll';
 
 const NavBar = () => {
-  const [selectedLang, setSelectedLang] = useState('');
+  const [selectedLang, setSelectedLang] = useState('en'); 
 
   const handleLangSelect = (lang) => {
-    setSelectedLang(lang); 
+    setSelectedLang(lang);
   };
 
   return (
-    <Navbar>
+    <Navbar className='navbar'>
       <Navbar.Brand href="#home">
         <img src="/assets/imgs/logo1.png" alt="Logo" className="logo" />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-nav" />
       <Navbar.Collapse id="navbar-nav">
-        <Nav className="ml-auto">
+        <Nav className="ms-auto align-items-center">
           <div className="language-selector">
-            <Button
-              className={`lang-button ${selectedLang === 'pt' ? 'active' : ''}`}
+            <span
+              className={`lang ${selectedLang === 'en' ? 'active' : ''}`}
+              onClick={() => handleLangSelect('en')}
+            >
+              EN
+            </span>
+            <span className="separator">|</span>
+            <span
+              className={`lang ${selectedLang === 'pt' ? 'active' : ''}`}
               onClick={() => handleLangSelect('pt')}
             >
               PT
-            </Button>
-            <Button
-              className={`lang-button ${selectedLang === 'sp' ? 'active' : ''}`}
-              onClick={() => handleLangSelect('sp')}
+            </span>
+            <span className="separator">|</span>
+            <span
+              className={`lang ${selectedLang === 'es' ? 'active' : ''}`}
+              onClick={() => handleLangSelect('es')}
             >
-              SP
-            </Button>
+              ES
+            </span>
           </div>
           <Nav.Link as={Link} to="contact" smooth={true} duration={500}>
-            <Button variant="primary">Contact</Button>
+            <Button variant="primary" className="ml-3">Contact</Button>
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
