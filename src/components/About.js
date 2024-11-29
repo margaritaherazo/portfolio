d57d70900072cc90a54c33e4f6e544d53f7db7d8
maggi <margarita.herazo.i@gmail.com>
2024-11-29 20:12:40 +0100

    adding about img and adjusting position

--- src/components/About.js
+++ src/components/About.js
@@ -2,10 +2,24 @@ import React from 'react';
 
 function About() {
     return (
-        <section id="about">
+        <section id="about" className="py-5">
             <div className="container">
-                <h2>About Me</h2>
-                <p>I am a passionate Front-End Developer with a knack for creating user-friendly and visually appealing websites.</p>
+                <div className="row align-items-center">
+                    <div className="col-md-6 d-flex justify-content-center">
+                        <img 
+                            src="/assets/imgs/foto.png" 
+                            alt="About Me" 
+                            className="img-fluid custom-img" 
+                        />
+                    </div>
+                    <div className="col-md-6">
+                        <h2>Web Designer</h2>
+                        <h2>Creative Developer</h2>
+                        <p>
+                            I am a passionate Front-End Developer with a knack for creating user-friendly and visually appealing websites.
+                        </p>
+                    </div>
+                </div>
             </div>
         </section>
     );
