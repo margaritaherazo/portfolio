4d6255c9edb9632045198648c4a3b313484c4ad2
maggi <margarita.herazo.i@gmail.com>
2024-12-02 06:39:42 -0300

    adding fotos to projects

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,10 +6,9 @@ function Portfolio() {
             <div className="container">
                 <h2 className="text-center">My Work</h2>
                 <div className="row">
-                    {/* Example Project */}
                     <div className="col-md-4 mb-4">
                         <div className="card">
-                            <img src="path/to/project-image.jpg" className="card-img-top" alt="Project" />
+                            <img src="assets/imgs/mi-ho.jpeg" className="card-img-top" alt="mi-ho Project" />
                             <div className="card-body">
                                 <h5 className="card-title">Project Name</h5>
                                 <p className="card-text">A brief description of the project.</p>
@@ -17,7 +16,26 @@ function Portfolio() {
                             </div>
                         </div>
                     </div>
-                    {/* Add more projects as needed */}
+                    <div className="col-md-4 mb-4">
+                        <div className="card">
+                            <img src="assets/imgs/acordaselva.jpeg" className="card-img-top" alt="acordaselva Project" />
+                            <div className="card-body">
+                                <h5 className="card-title">Project Name</h5>
+                                <p className="card-text">A brief description of the project.</p>
+                                <a href="https://github.com/username/project" className="btn btn-primary">View Project</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4 mb-4">
+                        <div className="card">
+                            <img src="assets/imgs/setecore.jpeg" className="card-img-top" alt="setes cores Project" />
+                            <div className="card-body">
+                                <h5 className="card-title">Project Name</h5>
+                                <p className="card-text">A brief description of the project.</p>
+                                <a href="https://github.com/username/project" className="btn btn-primary">View Project</a>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </section>
