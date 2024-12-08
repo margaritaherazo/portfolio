fb75e8163aa2cf674fa382429a105214fbebf23f
maggi <margarita.herazo.i@gmail.com>
2024-12-08 13:05:45 -0300

    adjusting skills styles for animation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,31 +1,35 @@
 import React from 'react';
 
 function Skills() {
+    const skills = [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "React",
+      "Node.js",
+      "Git",
+      "Bootstrap",
+      "jQuery",
+      "UI & UX Design",
+      "Figma",
+      "Problem-solving",
+      "Communication",
+      "Time Management",
+      "Teamwork",
+      "Empathy",
+      "Adaptability",
+    ];
+  
     return (
       <section id="skills">
         <div className="container">
-          <h2 className="text-center">Skills</h2>
-          <div className="row">
-            <div className="col-md-4">
-              <ul className="skills-list">
-                <li className="moving-skill">HTML</li>
-                <li className="moving-skill">CSS</li>
-                <li className="moving-skill">JavaScript</li>
-                <li className="moving-skill">React</li>
-                <li className="moving-skill">Node.js</li>
-                <li className="moving-skill">Git</li>
-                <li className="moving-skill">Bootstrap</li>
-                <li className="moving-skill">Jquery</li>
-                <li className="moving-skill">UI & UX Design</li>
-                <li className="moving-skill">Figma</li>
-                <li className="moving-skill">Problem-solving</li>
-                <li className="moving-skill">Communication</li>
-                <li className="moving-skill">Time Management</li>
-                <li className="moving-skill">Teamwork</li>
-                <li className="moving-skill">Empathy</li>
-                <li className="moving-skill">Adaptability</li>
-              </ul>
-            </div>
+          <h2>Skills</h2>
+          <div className="skills-wrapper">
+            {skills.map((skill, index) => (
+              <span className="moving-skill" key={index}>
+                {skill}
+              </span>
+            ))}
           </div>
         </div>
       </section>
