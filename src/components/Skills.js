0f7c0856981f080622010b09d5167916b257d09f
maggi <margarita.herazo.i@gmail.com>
2024-12-08 11:37:21 -0300

    changing skills list

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,37 +2,34 @@ import React from 'react';
 
 function Skills() {
     return (
-        <section id="skills" >
-            <div className="container">
-                <h2 className="text-center">Skills</h2>
-                <div className="row">
-                    {/* Technical Skills */}
-                    <div className="col-md-4">
-                        <h4>Technical Skills</h4>
-                        <ul>
-                            <li>HTML</li>
-                            <li>CSS</li>
-                            <li>JavaScript</li>
-                            <li>React</li>
-                            <li>Node.js</li>
-                            <li>Git</li>
-                            <li>Bootstrap</li>
-                        </ul>
-                    </div>
-                    {/* Soft Skills */}
-                    <div className="col-md-4">
-                        <h4>Soft Skills</h4>
-                        <ul>
-                            <li>Problem-solving</li>
-                            <li>Communication</li>
-                            <li>Time Management</li>
-                            <li>Teamwork</li>
-                        </ul>
-                    </div>
-                </div>
+      <section id="skills">
+        <div className="container">
+          <h2 className="text-center">Skills</h2>
+          <div className="row">
+            <div className="col-md-4">
+              <ul className="skills-list">
+                <li className="moving-skill">HTML</li>
+                <li className="moving-skill">CSS</li>
+                <li className="moving-skill">JavaScript</li>
+                <li className="moving-skill">React</li>
+                <li className="moving-skill">Node.js</li>
+                <li className="moving-skill">Git</li>
+                <li className="moving-skill">Bootstrap</li>
+                <li className="moving-skill">Jquery</li>
+                <li className="moving-skill">UI & UX Design</li>
+                <li className="moving-skill">Figma</li>
+                <li className="moving-skill">Problem-solving</li>
+                <li className="moving-skill">Communication</li>
+                <li className="moving-skill">Time Management</li>
+                <li className="moving-skill">Teamwork</li>
+                <li className="moving-skill">Empathy</li>
+                <li className="moving-skill">Adaptability</li>
+              </ul>
             </div>
-        </section>
+          </div>
+        </div>
+      </section>
     );
-}
-
-export default Skills;
+  }
+  
+  export default Skills;
