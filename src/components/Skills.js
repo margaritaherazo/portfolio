35669a0dec72f2f2614425a0729b41af3fdb9657
maggi <margarita.herazo.i@gmail.com>
2024-12-24 09:26:22 -0300

    adding trasnlations in my skills

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React from "react";
+import { useTranslation } from "react-i18next";
 
 function Skills() {
-    const skills = [
-      "HTML",
-      "CSS",
-      "JavaScript",
-      "React",
-      "Node.js",
-      "Git",
-      "Bootstrap",
-      "jQuery",
-      "UI & UX Design",
-      "Figma",
-      "Problem-solving",
-      "Communication",
-      "Time Management",
-      "Teamwork",
-      "Empathy",
-      "Adaptability",
-    ];
-  
-    return (
-      <section id="skills">
-        <div className="container">
-          <h2>Skills</h2>
-          <div className="skills-wrapper">
-            {skills.map((skill, index) => (
-              <span className="moving-skill" key={index}>
-                {skill}
-              </span>
-            ))}
-          </div>
+  const { t } = useTranslation();
+
+  const skills = [
+    t("skills.html"),
+    t("skills.css"),
+    t("skills.javascript"),
+    t("skills.react"),
+    t("skills.nodejs"),
+    t("skills.git"),
+    t("skills.bootstrap"),
+    t("skills.jquery"),
+    t("skills.uiux"),
+    t("skills.figma"),
+    t("skills.problemSolving"),
+    t("skills.communication"),
+    t("skills.timeManagement"),
+    t("skills.teamwork"),
+    t("skills.empathy"),
+    t("skills.adaptability"),
+  ];
+
+  return (
+    <section id="skills" className="py-5">
+      <div className="container">
+        <h2 className="text-center">{t("skills.title")}</h2>
+        <div className="skills-wrapper d-flex flex-wrap justify-content-center">
+          {skills.map((skill, index) => (
+            <span className="moving-skill mx-2 my-1" key={index}>
+              {skill}
+            </span>
+          ))}
         </div>
-      </section>
-    );
-  }
-  
-  export default Skills;
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
